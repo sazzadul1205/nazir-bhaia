8546e0e8d08aceb402a739df5c9c29cf6fb28e75
Pritom <psazzadul@gmail.com>
2026-07-21 11:28:32 +0600

    - Added Tailwind .config for intelligence - Added React icons

--- src/layouts/PublicLayout.jsx
+++ src/layouts/PublicLayout.jsx
@@ -1,8 +1,14 @@
+import { FaPhoneAlt } from "react-icons/fa";
 import { Outlet } from "react-router-dom";
 
 const PublicLayout = () => {
   return (
     <div className="bg-white min-h-screen">
+      <div className="bg-green-500 p-2 flex">
+        <div className="bg-white rounded-full" >
+          <FaPhoneAlt className="text-black" />
+        </div>
+      </div>
       <Outlet />
     </div>
   );
